aace2c9505395d94683ca9309693f74cd7885918
Serenity Phillips <sphillips@unc.edu>
2026-05-25 15:51:54 -0400

    backend: add firebase and dotenv, update to ES modules

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,14 @@
-// Import the express module
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
-// Create an instance of the express application
 const app = express();
+const port = process.env.PORT || 5000;
 
-// Specify a port number for the server
-const port = 5000;
-
-// use middleware to parse json request bodies
 app.use(cors());
 app.use(express.json());
 
-// Start the server and listen to the port
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
